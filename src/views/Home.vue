7187d1c92a64ae5728bd6a549a56f93f3467aea7
zzjjiang <zoujone@163.com>
2020-10-06 17:44:24 +0800

    home alter

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,45 @@
 <template>
     <div>
-        <h3>this is Home</h3>
+      <h3>this is Home</h3>
+      <ul>
+        <li v-for="(item,index) in lists" :key="index" @click="choose(index)" :class="{active:index==current&&current!==''}">
+          {{ item }}
+        </li>
+      </ul>
+      <button @click="add()">添加</button>
+      <ul>
+        <li v-for="(item,index) in targets" :key="index">
+          {{ item }}
+        </li>
+      </ul>
     </div>
 </template>
+
 <script>
 export default {
   name: 'home',
   data () {
     return {
-      lists: [1, 2, 3, 4, 5, 6, 7, 8, 9]
+      current: '',
+      lists: [1, 2, 3, 4, 5, 6, 7, 8, 9],
+      targets: []
+    }
+  },
+  methods: {
+    choose (index) {
+      this.current = index
+    },
+    add () {
+      if (this.current === '') { return }
+      this.targets.push(this.lists[this.current])
+      this.current = ''
     }
   }
 }
 </script>
-<style>
 
+<style>
+li.active {
+  background: green;
+}
 </style>
